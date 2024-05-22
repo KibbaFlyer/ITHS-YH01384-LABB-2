349adbb499664014d2d6936d416c13fd3c959db4
KibbaFlyer <kristoffer.flygare@icloud.com>
2024-05-22 19:35:45 +0200

    Adding memoization and reducer

--- Labb2/src/pages/CatComponent.tsx
+++ Labb2/src/pages/CatComponent.tsx
@@ -1,37 +1,55 @@
-import { useState, useEffect } from 'react';
+import { useState, useReducer, useMemo } from 'react';
 import fetchCatPic from '../api/catpic';
 import { Button } from '../styling/LyricsComponentStyling';
 
+interface State {
+    count: number;
+}
+
+type Action = {
+    type: "increment";
+}
+
+function reducerFunction(state: State, action: Action) {
+    switch (action.type) {
+        case "increment":
+            return { count: state.count + 1 }
+        default:
+            return state;
+    }
+}
+
 const CatComponent = () => {
     const [picArray, setPicArray] = useState<string[]>([]);
+    const [state, dispatch] = useReducer(reducerFunction, { count: 0 })
+    console.log("initializing")
 
     const fetchAndSetPic = async () => {
         const pic = await fetchCatPic();
         if (pic) {
             setPicArray(prevArray => [...prevArray, pic.toString()]);
+            dispatch({ type: "increment" });
         }
     };
 
-    useEffect(() => {
-        fetchAndSetPic();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const memoizedPicArray = useMemo(() => picArray, [picArray]);
 
     return (
         <div>
             <Button style={{
-                    margin:"30px"
-                }}
+                margin: "30px"
+            }}
                 onClick={fetchAndSetPic}>Generate more!</Button>
+            <p>Amount of cats {state.count}</p>
             <div
                 style={{
                     position: "fixed",
                     overflow: "auto",
                     height: "100%",
-                    padding:"30px"
+                    padding: "30px"
                 }}
             >
-                {picArray.map((pic, index) => {
+                {memoizedPicArray.map((pic, index) => {
                     return <img key={index} src={pic} style={{ padding: "30px" }} />;
                 })}
             </div>
